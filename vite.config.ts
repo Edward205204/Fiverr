06c64e6b04f7d58858a38eba61bc990bd3d9dc29
Edward205204 <khoa123kd56cn@gmail.com>
2025-06-21 13:03:54 +0700

    chore: config shacnUI

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,17 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    port: 3000
+    port: 3009
   },
   css: {
     devSourcemap: true
   },
   resolve: {
     alias: {
-      '~': path.resolve(__dirname, './src')
+      '@': path.resolve(__dirname, './src')
     }
   }
 });
