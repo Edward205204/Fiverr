2f1dc5fa06a0788e3aa84762dde9958c503de4ab
Edward205204 <khoa123kd56cn@gmail.com>
2025-06-22 15:14:22 +0700

    feat: update footer

--- src/components/shared/footer.tsx
+++ src/components/shared/footer.tsx
@@ -9,16 +9,36 @@ export default function Footer() {
         <div>
           <h3 className='font-semibold mb-2'>Categories</h3>
           <ul className='space-y-3'>
-            <li className='cursor-pointer'>Graphics & Design</li>
-            <li className='cursor-pointer'>Digital Marketing</li>
-            <li className='cursor-pointer'>Writing & Translation</li>
-            <li className='cursor-pointer'>Video & Animation</li>
-            <li className='cursor-pointer'>Music & Audio</li>
-            <li className='cursor-pointer'>Programming & Tech</li>
-            <li className='cursor-pointer'>Data</li>
-            <li className='cursor-pointer'>Business</li>
-            <li className='cursor-pointer'>Lifestyle</li>
-            <li className='cursor-pointer'>Sitemap</li>
+            <li className='hover:cursor-pointer'>
+              <span className='hover:underline '>Graphics & Design</span>
+            </li>
+            <li className='hover:cursor-pointer'>
+              <span className='hover:underline '>Digital Marketing</span>
+            </li>
+            <li className='hover:cursor-pointer'>
+              <span className='hover:underline '>Writing & Translation</span>
+            </li>
+            <li className='hover:cursor-pointer'>
+              <span className='hover:underline '>Video & Animation</span>
+            </li>
+            <li className='hover:cursor-pointer'>
+              <span className='hover:underline '>Music & Audio</span>
+            </li>
+            <li className='hover:cursor-pointer'>
+              <span className='hover:underline '>Programming & Tech</span>
+            </li>
+            <li className='hover:cursor-pointer'>
+              <span className='hover:underline '>Data</span>
+            </li>
+            <li className='hover:cursor-pointer'>
+              <span className='hover:underline '>Business</span>
+            </li>
+            <li className='hover:cursor-pointer'>
+              <span className='hover:underline '>Lifestyle</span>
+            </li>
+            <li className='hover:cursor-pointer'>
+              <span className='hover:underline '>Sitemap</span>
+            </li>
           </ul>
         </div>
 
@@ -26,13 +46,27 @@ export default function Footer() {
         <div>
           <h3 className='font-semibold mb-2'>About</h3>
           <ul className='space-y-3'>
-            <li className='cursor-pointer'>Careers</li>
-            <li className='cursor-pointer'>Press & News</li>
-            <li className='cursor-pointer'>Partnerships</li>
-            <li className='cursor-pointer'>Privacy Policy</li>
-            <li className='cursor-pointer'>Terms of Service</li>
-            <li className='cursor-pointer'>Intellectual Property Claims</li>
-            <li className='cursor-pointer'>Investor Relations</li>
+            <li className='hover:cursor-pointer'>
+              <span className='hover:underline '>Careers</span>
+            </li>
+            <li className='hover:cursor-pointer'>
+              <span className='hover:underline '>Press & News</span>
+            </li>
+            <li className='hover:cursor-pointer'>
+              <span className='hover:underline '>Partnerships</span>
+            </li>
+            <li className='hover:cursor-pointer'>
+              <span className='hover:underline '>Privacy Policy</span>
+            </li>
+            <li className='hover:cursor-pointer'>
+              <span className='hover:underline '>Terms of Service</span>
+            </li>
+            <li className='hover:cursor-pointer'>
+              <span className='hover:underline '>Intellectual Property Claims</span>
+            </li>
+            <li className='hover:cursor-pointer'>
+              <span className='hover:underline '>Investor Relations</span>
+            </li>
           </ul>
         </div>
 
@@ -40,10 +74,18 @@ export default function Footer() {
         <div>
           <h3 className='font-semibold mb-2'>Support</h3>
           <ul className='space-y-3'>
-            <li className='cursor-pointer'>Help & Support</li>
-            <li className='cursor-pointer'>Trust & Safety</li>
-            <li className='cursor-pointer'>Selling on Fiverr</li>
-            <li className='cursor-pointer'>Buying on Fiverr</li>
+            <li className='hover:cursor-pointer'>
+              <span className='hover:underline '>Help & Support</span>
+            </li>
+            <li className='hover:cursor-pointer'>
+              <span className='hover:underline '>Trust & Safety</span>
+            </li>
+            <li className='hover:cursor-pointer'>
+              <span className='hover:underline '>Selling on Fiverr</span>
+            </li>
+            <li className='hover:cursor-pointer'>
+              <span className='hover:underline '>Buying on Fiverr</span>
+            </li>
           </ul>
         </div>
 
@@ -51,15 +93,33 @@ export default function Footer() {
         <div>
           <h3 className='font-semibold mb-2'>Community</h3>
           <ul className='space-y-3'>
-            <li className='cursor-pointer'>Events</li>
-            <li className='cursor-pointer'>Blog</li>
-            <li className='cursor-pointer'>Forum</li>
-            <li className='cursor-pointer'>Community Standards</li>
-            <li className='cursor-pointer'>Podcast</li>
-            <li className='cursor-pointer'>Affiliates</li>
-            <li className='cursor-pointer'>Invite a Friend</li>
-            <li className='cursor-pointer'>Become a Seller</li>
-            <li className='cursor-pointer'>Fiverr Elevate</li>
+            <li className='hover:cursor-pointer'>
+              <span className='hover:underline '>Events</span>
+            </li>
+            <li className='hover:cursor-pointer'>
+              <span className='hover:underline '>Blog</span>
+            </li>
+            <li className='hover:cursor-pointer'>
+              <span className='hover:underline '>Forum</span>
+            </li>
+            <li className='hover:cursor-pointer'>
+              <span className='hover:underline '>Community Standards</span>
+            </li>
+            <li className='hover:cursor-pointer'>
+              <span className='hover:underline '>Podcast</span>
+            </li>
+            <li className='hover:cursor-pointer'>
+              <span className='hover:underline '>Affiliates</span>
+            </li>
+            <li className='hover:cursor-pointer'>
+              <span className='hover:underline '>Invite a Friend</span>
+            </li>
+            <li className='hover:cursor-pointer'>
+              <span className='hover:underline '>Become a Seller</span>
+            </li>
+            <li className='hover:cursor-pointer'>
+              <span className='hover:underline '>Fiverr Elevate</span>
+            </li>
           </ul>
         </div>
 
@@ -67,15 +127,33 @@ export default function Footer() {
         <div>
           <h3 className='font-semibold mb-2'>More From Fiverr</h3>
           <ul className='space-y-3'>
-            <li className='cursor-pointer'>Fiverr Business</li>
-            <li className='cursor-pointer'>Fiverr Pro</li>
-            <li className='cursor-pointer'>Fiverr Studios</li>
-            <li className='cursor-pointer'>Fiverr Logo Maker</li>
-            <li className='cursor-pointer'>Fiverr Guides</li>
-            <li className='cursor-pointer'>Get Inspired</li>
-            <li className='cursor-pointer'>ClearVoice</li>
-            <li className='cursor-pointer'>AND CO</li>
-            <li>Learn</li>
+            <li className='hover:cursor-pointer'>
+              <span className='hover:underline '>Fiverr Business</span>
+            </li>
+            <li className='hover:cursor-pointer'>
+              <span className='hover:underline '>Fiverr Pro</span>
+            </li>
+            <li className='hover:cursor-pointer'>
+              <span className='hover:underline '>Fiverr Studios</span>
+            </li>
+            <li className='hover:cursor-pointer'>
+              <span className='hover:underline '>Fiverr Logo Maker</span>
+            </li>
+            <li className='hover:cursor-pointer'>
+              <span className='hover:underline '>Fiverr Guides</span>
+            </li>
+            <li className='hover:cursor-pointer'>
+              <span className='hover:underline '>Get Inspired</span>
+            </li>
+            <li className='hover:cursor-pointer'>
+              <span className='hover:underline '>ClearVoice</span>
+            </li>
+            <li className='hover:cursor-pointer'>
+              <span className='hover:underline '>AND CO</span>
+            </li>
+            <li className='hover:cursor-pointer'>
+              <span className='hover:underline '>Learn</span>
+            </li>
           </ul>
         </div>
       </div>
